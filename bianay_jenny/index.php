50fa1dfe44ea22589bd712db899092ce68146985
BIANAY Jenny <rsma.simplon21@gmail.com>
2021-04-08 10:33:28 -0400

    mon porfolio

---
+++ bianay_jenny/index.php
@@ -0,0 +1,169 @@
+<?php
+ 
+ $page = porfolio
+
+ ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+ <title>Porfolio</title>
+
+ <link rel="stylesheet" href="css/style.css"> 
+ <link href="https://fonts.googleapis.com/css2?family=Henny+Penny&display=swap" rel="stylesheet"> 
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  </head>
+  <body>
+  
+<div class="container" >
+<div class="row">
+<div class="col-7"> 
+  <h2> PORFOLIO</h2>
+ </div> <!--fin col-7-->
+
+ <div class="col-5">
+          <ul class="nav nav-h">
+            <li class="nav-item">
+              <a class="nav-link active" href="home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="work">Work</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="blog">Blog</a>
+            </li>
+            <button type="button" class="btn btn-outline-primary">Hire Me!</button>
+          </ul>
+      
+</div> <!--fin col-5-->
+
+</div><!--fin row image-->
+</div> <!--fin container -->
+
+<div class="container" id="first">
+<div class="row">
+  <div class="col-5">
+  <img  src="img/me.jpg" alt="image homme" id="homme">
+  </div><!-- fin col-5-->
+ 
+  <div class="col-7" id="para">
+  <h2 id="h2">Hello, i'm Dejee</h2>
+
+  <h1 class="h1"><B>Expert in Graphic <br> Design &Development </B></h1>
+
+  <p class="texte">Currently available for select projects, collaborations and consulting. Currently <br> available for select projects,collaborations and consulting.</p>
+  <button type="button" class="btn btn-primary">SEE MY WORK</button>
+ </div><!-- fin col-7-->
+</div><!--fin row image -->
+</div><!--fin container image -->
+
+
+
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+  <p id="titre">SERVICES</p>
+    </div><!--fin col 12-->
+    </div><!-- fin row-->
+   
+    <div class="row"id="legende">
+     
+    <div class="col-3">
+      <img src="img/icone_pot.png" alt="">
+ <h2><B>WEB DESIGN</B></h2>
+ <p>well we're movin on up to the east<br>side.To deluxe apartment </p>
+      </div><!--fin col 3-->
+
+      <div class="col-3" id="diamant">
+ <img src="img/icone_diamant.png" alt="">
+ <h2><B>MOBIL APP</B></h2>
+<p>Tracking immunizations has never <br> been easier or more secure...</p>
+
+      </div><!--fin col 3-->
+
+    <div class="col-3">
+     <img src="img/icone_batton_magic.png" alt="">
+ <h2><B>WEB DESIGN</B></h2>
+ <p>well we're movin on up to the east<br>side.To deluxe apartment </p>
+    </div><!--fin col 3-->
+   
+  </div><!-- fin row-->
+</div><!--fin container-->
+
+<div class="container" id="gris">
+  <div class="row">
+<div class="col-12">
+  <p id="title">PORTFOLIO</p>
+</div> <!--fin col-12-->
+  </div><!--fin row-->
+ 
+  <div class="row" id="carreg">
+    <div class="col-3">
+ <img src="img/image_projet.jpg" alt="" width=300px" height="250px">
+    </div><!--fin div-->
+
+    <div class="col-3">
+    <img src="img/image_projet.jpg" alt="" width="300px" height="250px">
+    </div><!--fin div-->
+
+    <div class="col-3">
+    <img src="img/image_projet.jpg" alt="" width="300px" height="250px">
+    </div><!--fin div-->
+    
+    </div><!--fin row-->
+
+    <div class="row" id="carregb">
+    
+    <div class="col-3" >
+    <img src="img/image_projet.jpg" alt="" width="300px" height="250px">
+    </div><!--fin div-->
+
+    <div class="col-3">
+    <img src="img/image_projet.jpg" alt="" width="300px" height="250px">
+    </div><!--fin div-->
+
+    <div class="col-3">
+    <img src="img/background_titre_theme_projet.png" alt="" width="300px" height="250px">
+    </div><!--fin div-->
+  
+  </div><!--finrow-->
+
+
+</div><!--fin container-->
+
+<div class="conatainer" id="make">
+<div class="row">
+<div class="col-12">
+<h1><B>Make It Awesome!</B></h1>
+<p>There's nothing we wont try.  Never heard the word impossible. This time there's no stopping us. <br>  the year is 1987 and NASAS  lauches the last of americas deep space probes.</p>
+<button type="button" class="btn btn-outline-primary">Hire Me!</button>
+</div><!--fin div -->
+
+
+
+</div><!--div.row-->
+</div><!--fin container-->
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
